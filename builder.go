924a60b60db88cdc5eb50cedc9c5ade1b3015302
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-02-27 15:33:24 +0700

    add New() for create local logger

--- builder.go
+++ builder.go
@@ -5,3 +5,11 @@ var Global *Logger = &Logger{
 	level:   INFO,
 	setting: &DefaultSetting,
 }
+
+// New will create new logger struct
+func New(level Level, setting Setting) *Logger {
+	return &Logger{
+		level:   level,
+		setting: &setting,
+	}
+}
